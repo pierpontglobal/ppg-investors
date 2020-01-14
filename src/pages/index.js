b4333c6c016a9fc00d50e2edb634776bbedf502e
Hector A. Acosta <hector.acosta.pozo@gmail.com>
2020-01-13 22:57:09 -0400

    Test

--- src/pages/index.js
+++ src/pages/index.js
@@ -27,15 +27,19 @@ const Money = styled.div`
 `;
 
 function parseJwt(token) {
-  if (token !== undefined) {
-    var base64Url = token.split('.')[1];
-    var base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
-    var jsonPayload = decodeURIComponent(atob(base64).split('').map(function (c) {
-      return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
-    }).join(''));
+  try {
+    if (token !== undefined) {
+      var base64Url = token.split('.')[1];
+      var base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
+      var jsonPayload = decodeURIComponent(atob(base64).split('').map(function (c) {
+        return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
+      }).join(''));
 
-    return JSON.parse(jsonPayload);
-  } else {
+      return JSON.parse(jsonPayload);
+    } else {
+      return {};
+    }
+  } catch (e) {
     return {};
   }
 };
@@ -196,51 +200,56 @@ const IndexPage = ({ component: Component, ...rest }) => {
   };
 
   const userData = parseJwt(qs.parse(rest.location.search).token);
-
   const user = DummyUsers[userData.pos - 34];
 
-  return (
-    <GatsdbyLayout>
-      <Layout style={{
-        width: '80%',
-        minWidth: '320px',
-        maxWidth: '960px',
-        paddingTop: '60px',
-        height: '100vh',
-        paddingBottom: '60px',
-        margin: `0 auto`
-      }}>
-        <SEO title="Home" />
-        <div>
-          <h1 style={{
-            fontWeight: '100',
-          }}>
-            Hello {user.name}
-          </h1>
-          <h4 style={{ color: 'darkgrey', fontWeight: '300' }}>Invest in your future!</h4>
-          <div style={{ width: '100%', display: 'flex', justifyContent: 'flex-start', marginBottom: '24px' }}>
-            <InvestmentBox>
-              Initial Investment
+  if (user === undefined) {
+    return <div style={{ position: 'absolute', top: '0', bottom: '0', left: '0', right: '0', margin: 'auto', textAlign: 'center', height: '60px', fontSize: '60px' }}>
+      404 NOT FOUND
+    </ div>
+  } else {
+    return (
+      <GatsdbyLayout>
+        <Layout style={{
+          width: '80%',
+          minWidth: '320px',
+          maxWidth: '960px',
+          paddingTop: '60px',
+          height: '100vh',
+          paddingBottom: '60px',
+          margin: `0 auto`
+        }}>
+          <SEO title="Home" />
+          <div>
+            <h1 style={{
+              fontWeight: '100',
+            }}>
+              Hello {user.name}
+            </h1>
+            <h4 style={{ color: 'darkgrey', fontWeight: '300' }}>Invest in your future!</h4>
+            <div style={{ width: '100%', display: 'flex', justifyContent: 'flex-start', marginBottom: '24px' }}>
+              <InvestmentBox>
+                Initial Investment
               <Money>{user.initialInvestment}</Money>
-            </InvestmentBox>
-            <InvestmentBox>
-              Current investment value
+              </InvestmentBox>
+              <InvestmentBox>
+                Current investment value
               <Money>{user.currentInvestment}</Money>
-            </InvestmentBox>
+              </InvestmentBox>
+            </div>
+            <Line
+              width={100}
+              height={50}
+              data={data}
+              grid
+              options={options}
+            />
+            <h3 style={{ marginTop: '12px' }}>Details</h3>
+            <Table columns={columns} dataSource={user.detailsData} pagination={{ disabled: true, hideOnSinglePage: true }} />
           </div>
-          <Line
-            width={100}
-            height={50}
-            data={data}
-            grid
-            options={options}
-          />
-          <h3 style={{ marginTop: '12px' }}>Details</h3>
-          <Table columns={columns} dataSource={user.detailsData} pagination={{ disabled: true, hideOnSinglePage: true }} />
-        </div>
-      </Layout>
-    </GatsdbyLayout>
-  )
+        </Layout>
+      </GatsdbyLayout>
+    )
+  }
 }
 
 export default IndexPage
